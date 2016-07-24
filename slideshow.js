c2adf991e9bb96b3b90a76284757b6b667d56904
Henkeee <henrik.elmer@live.se>
2016-07-24 20:10:39 +0200

    fixed navigation dots on slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,23 +1,53 @@
-var slideImgNbr = 0;
-showDivs(slideImgNbr);
+//index for displayed division
+var slideDivNbr = 0;
+//slide Elements
+var slides = document.getElementsByClassName("slide");
+//navigation dot Elements
+var dots = document.getElementsByClassName("dot");
 
-function changeDivs(n) {
-    showDivs(slideImgNbr += n);
+//displays initial slide
+showDivs(slideDivNbr);
+
+
+
+//returns index for displayed division, currently unused
+function checkActive(){
+    return slideDivNbr;
 }
 
+//changes displayed division in slideshow, d = number of steps
+function changeDivs(d) {
+    showDivs(slideDivNbr += d);
+}
+
+//displays division with index n, loops within number of slides
 function showDivs(n) {
-    slideImgNbr=n;
-    var i;
-    var slides = document.getElementsByClassName("slide");
+    slideDivNbr=n;
+    
     if(n>slides.length-1) {
-        slideImgNbr=0;
+        slideDivNbr=0;
     } 
     if (n<0) {
-        slideImgNbr=slides.length-1;
+        slideDivNbr=slides.length-1;
     }
+    //hide all slides
+    var i;
     for(i=0;i<slides.length;i++){
         slides[i].style.display="none";
     }
-    slides[slideImgNbr].style.display = "block";
+    //display slide n
+    slides[slideDivNbr].style.display = "block";
     
+    //highlight active dot
+    highlightDot(slideDivNbr);
 }
+
+//highlights active dot
+function highlightDot(n) {
+    var i;
+    for(i=0;i<dots.length;i++) {
+        dots[i].classList.remove("dot_active");
+    }
+    dots[n].classList.toggle("dot_active");
+}
+
