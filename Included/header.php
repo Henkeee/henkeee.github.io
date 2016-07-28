2491f96f9aa7baa8a49e65f9eb84498e02fa3c96
Henkeee <henrik.elmer@live.se>
2016-07-28 21:58:59 +0200

    fixed links

--- Included/header.php
+++ Included/header.php
@@ -3,13 +3,13 @@
         <button id="header_dropdown_icon" class="dropdown_button noselect" onclick="showDropdown(1)">&#9776;</button>
         
             <div id="header_dropdown_content" class="dropdown_content">
-                <a href="avoidthefloor.html" class="dropdown_item">Avoid the Floor</a>
+                <a href="avoidthefloor.php" class="dropdown_item">Avoid the Floor</a>
                 
-                <a href="index.html" class="dropdown_item">Link 2</a>
+                <a href="genericleaf.php" class="dropdown_item">Blank</a>
                 
                 <a href="index.html" class="dropdown_item">Link 3</a>
             </div> 
         
-        <a href="index.html"><img id="main_logga" src="Pictures/leaf-favicon.PNG" width="100px"></a>
+        <a href="index.php"><img id="main_logga" src="Pictures/leaf-favicon.PNG" width="100px"></a>
 
     </div>
